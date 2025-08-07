22005ce1c258ce6bafb73da135302b13241de624
ramymonazaa@gmail.com <53308887+ramymonazaa@users.noreply.github.com>
2025-08-07 12:44:16 +0300

    Update caeaser.py

--- substitution cipher/caeaser.py
+++ substitution cipher/caeaser.py
@@ -1,26 +1,18 @@
-def caeser_encrypt(plain_text,shift):
-    cipher_text=""
-    for ch in plain_text:
+def caeser_encrypt_decrypt(text,shift):
+    result=""
+    for ch in text:
         if ch.isalpha():
             base=ord('A') if ch.isupper() else ord('a')
-            cipher_text+=chr(base+((ord(ch)-base+shift+26)%26))
+            result+=chr(base+((ord(ch)-base+shift+26)%26))
         else:
-            cipher_text+=ch
-    return cipher_text
+            result+=ch
+    return result
 
-def caeser_decrypt(cipher_text,shift):
-    plain_text=""
-    for ch in cipher_text:
-        if ch.isalpha():
-            base=ord('A') if ch.isupper() else ord('a')
-            plain_text+=chr(base+((ord(ch)-base-shift+26)%26))
-        else:
-            plain_text+=ch
-    return plain_text
 
 shift=3
 text="Ramy"
-ciphertext=caeser_encrypt(text,shift)
+ciphertext=caeser_encrypt_decrypt(text,shift)
 print("ciphertext:",ciphertext)
-plaintext=caeser_decrypt(ciphertext,shift)
+plaintext=caeser_encrypt_decrypt(ciphertext,shift)
+
 print("plaintext:",plaintext)
